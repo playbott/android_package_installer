876fbdca219b04ff97468839ef67a93b57ca7a3e
Rovshik <rovsh.dev@gmail.com>
2022-12-20 12:42:20 +0500

    added description for installApk method

--- lib/android_package_installer.dart
+++ lib/android_package_installer.dart
@@ -4,6 +4,11 @@ export 'package:android_package_installer/src/method_channel.dart';
 export 'package:android_package_installer/src/enums.dart';
 
 class AndroidPackageInstaller {
+  /// Installs apk file using Android Package Manager.
+  /// Creates Package Installer session, then displays a dialog to confirm the installation.
+  /// After installation process is completed, the session is closed.
+  /// [apkFilePath] - the path to the apk package file. Example: /sdcard/Download/app.apk
+  /// Returns session result code.
   static Future<int?> installApk({required String apkFilePath}) {
     Future<int?> code = AndroidPackageInstallerPlatform.instance.installApk(apkFilePath);
     return code;
